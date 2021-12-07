c5b4b355e834652c0a68bfed7da57b2db7926b32
Simran Makhija <44701229+SimranMakhija7@users.noreply.github.com>
2021-12-07 17:51:44 +0530

    feat: create app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("Translation will appear here...");
+
+  const inputChangeHandler = (event) => {
+    var inputVal = event.target.value;
+    setInput(inputVal);
+    var food = getFood(inputVal);
+    setOutput(food);
+  };
+
+  const getFood = (emoji) => {
+    if (foodDict[emoji]) {
+      return foodDict[emoji];
+    } else {
+      return "Unavailable in dictionary!";
+    }
+  };
+
+  const selectEmoji = (event) => {
+    var selectedEmoji = event.target.value;
+    setInput(selectedEmoji);
+    var food = getFood(selectedEmoji);
+    setOutput(food);
+  };
+
+  var foodDict = {
+    "🍎": "Apple",
+    "🥭": "Mango",
+    "🍕": "Pizza",
+    "☕️": "Coffee",
+    "🍩": "Donut",
+    "🍪": "Cookie"
+  };
+
+  var options = Object.keys(foodDict).map((k, i) => {
+    return (
+      <button value={k} onClick={selectEmoji} className="emojiButton" key={i}>
+        {k}
+      </button>
+    );
+  });
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Foood Interpreter</h1>
+      <input
+        value={input}
+        id="userInput"
+        placeholder="Enter Food Emoji or choose from below"
+        onChange={inputChangeHandler}
+      />
+      <div id="output">{output}</div>
+      <div>{options}</div>
     </div>
   );
 }
